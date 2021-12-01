fb84354c6fc165fcc3af079dc50b985e99e602ec
radjavi <radjavi@hotmail.com>
2021-12-01 06:12:18 +0100

    day1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,31 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var increases = 0
+        for (i in 1 until input.size) {
+            if (input[i].toInt() > input[i-1].toInt()) increases++
+        }
+        return increases
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val list = mutableListOf<List<Int>>()
+        for (i in 0..input.size-3) {
+            list.add(
+                listOf(
+                    input[i].toInt(),
+                    input[i+1].toInt(),
+                    input[i+2].toInt(),
+                )
+            )
+        }
+        val sums = list.map { it.sum().toString() }
+        return part1(sums)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
